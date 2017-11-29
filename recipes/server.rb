d7bc33057eef82fa5beda37cdce8d2ffcbe26cb9
varunhanumath <varungudimetla@gmail.com>
2017-11-29 18:58:05 +0000

    first commit

---
+++ recipes/server.rb
@@ -0,0 +1,11 @@
+powershell_script 'Install IIS' do
+code 'Add-WindowsFeature Web-Server'
+action :run
+end
+file 'C:\inetpub\wwwroot\Default.htm' do
+content '<h1>Hello, World!</h1>'
+end
+
+service 'w3svc' do
+action [ :enable, :start ]
+end

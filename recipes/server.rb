f18274a1296269c0838754a5fbad2ece732e6e76
varunhanumath <varungudimetla@gmail.com>
2017-11-29 19:59:43 +0000

    version 0.2.0

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,11 @@ code 'Add-WindowsFeature Web-Server'
 action :run
 end
 file 'C:\inetpub\wwwroot\Default.htm' do
-content '<h1>Hello, World!</h1>'
+content "<h1>Hello, World!</h1>
+<h2>PLATFORM: #{node['platform']}</h2>
+<h2>HOSTNAME: #{node['hostname']}</h2>
+<h2>MEMORY: #{node['memory']['total']}</h2>
+<h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
 end
 
 service 'w3svc' do
